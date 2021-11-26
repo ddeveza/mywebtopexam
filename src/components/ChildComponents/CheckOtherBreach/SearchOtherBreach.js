5311acd6fa51d47bab8f6a13c7b013b2b55faf4b
Dennis Deveza <Dennis.Deveza@onsemi.com>
2021-11-26 09:26:52 +0800

    added search box for other email address

--- src/components/ChildComponents/CheckOtherBreach/SearchOtherBreach.js
+++ src/components/ChildComponents/CheckOtherBreach/SearchOtherBreach.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal, Backdrop, Box, Button, Typography } from "@material-ui/core";
 import emailLogo from "../../../logo/Assets/icons8-mail.png";
-
+import phoneLogo from "../../../logo/Assets/icons8-touchscreen.png";
+import SearchIcon from "@material-ui/icons/Search";
 
 const SearchOtherBreach = (props) => {
+  const [onClickSytle, setOnClickSytle] = useState();
+  const searchHandle = () => {};
   const modalStyle = {
     position: "absolute",
     top: "60%",
@@ -15,7 +18,7 @@ const SearchOtherBreach = (props) => {
     display: "flex",
     flexDirection: "column",
     p: 1,
-    paddingTop: "0px",
+    paddingTop: "1px",
     boxSizing: "content-box",
     WebkitFilter: "drop-shadow(2px 2px 5px  rgb(120 196 217))",
     borderRadius: "1%",
@@ -23,9 +26,17 @@ const SearchOtherBreach = (props) => {
   };
 
   const mainTitle = {
-    fontSize: '36px',
-    fontWeight: '700'
-  }
+    fontSize: "40px",
+    color: "rgba(42, 129, 163, 1)",
+  };
+
+  const searchBox = {
+    backgroundColor: "rgb(242, 242, 242)",
+    border: 0,
+    outline: "none",
+    width: "410px",
+    padding: "10px",
+  };
 
   return (
     <Modal
@@ -47,19 +58,89 @@ const SearchOtherBreach = (props) => {
               flexDirection: "row",
               marginTop: "10px",
               marginLeft: "10px",
+              alignItems: "center",
             }}
           >
-            <Box>
-              <img src={emailLogo} alt="search logo" />
+            <Box sx={{ display: "flex", marginLeft: "19px" }}>
+              <img
+                src={props.desc.search("email") !== -1 ? emailLogo : phoneLogo}
+                alt="search logo"
+                style={{ width: "130px", height: "130px" }}
+              />
             </Box>
-            <Box>
-              <Typography sx={mainTitle}>{props.desc.toUpperCase()}</Typography>
+            <Box sx={{ display: "flex", paddingLeft: "30px" }}>
+              <Typography style={mainTitle}>
+                {props.desc.toUpperCase()}
+              </Typography>
             </Box>
           </Box>
-          <Box>About</Box>
-          <Box>Search</Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignSelf: "center",
+              flexDirection: "column",
+            }}
+          >
+            {props.desc.search("email") !== -1 ? (
+              <Typography
+                style={{ fontSize: "20px", color: "rgba(112, 112, 112, 1)" }}
+              >
+                Enter email address to be checked
+              </Typography>
+            ) : (
+              <>
+                {" "}
+                <Typography
+                  style={{ fontSize: "20px", color: "rgba(112, 112, 112, 1)" }}
+                >
+                  Enter the phone number to be checked including the country
+                  code
+                </Typography>
+                <Typography
+                  style={{ fontSize: "20px", color: "rgba(112, 112, 112, 1)" }}
+                >
+                  Eg:+61412333555`
+                </Typography>
+              </>
+            )}
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignSelf: "center",
+              marginTop: "20px",
+              border: "1px solid rgba(112, 112, 112, 1) ",
+              width: "auto",
+              height: "60px",
+            }}
+          >
+            <input
+              type={props.desc.search("email") !== -1 && "email"}
+              style={searchBox}
+            />
+            <Button
+              style={{
+                
+                padding:'0px',
+                "&:hover": { backgroundColor: "rgb(242 242 242 / 38%)" },
+              }}
+            >
+              <SearchIcon
+                onClick={searchHandle}
+                style={{
+                  cursor: "pointer",
+                  width: "60px",
+                  height: "auto",
+                  color: "#5181b1",
+                  borderLeft: "1px solid rgba(112, 112, 112, 1)",
+                  padding:'5px'
+                }}
+              />
+            </Button>
+          </Box>
           <Box>
-            <Button onClick={props.handleToggle}>CLOSE</Button>
+            <Button onClick={props.handleToggle} style={{}}>CLOSE</Button>
           </Box>
         </Box>
       </>
